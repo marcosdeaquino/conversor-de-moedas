7676b43062b2ac6c0af047b7e9fdaba744e04c5d
marcosdeaquino <marcos.deaquino2018@gmail.com>
2023-10-27 16:17:56 -0300

    meu commit

---
+++ scripts.js
@@ -0,0 +1,7 @@
+const convertButton = document.querySelector(".convert-Button")
+
+function convertValues(){
+    console.log("Funcionou")
+}
+
+convertButton.addEventListener("click", convertValues)
